12ebc2701ba9d12fee9cd125339572ce1b08df7d
lu-carvalho <91640075+lu-carvalho@users.noreply.github.com>
2022-01-06 22:47:33 +0000

    Thu, Jan 6, 2022, 10:47 PM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -4,5 +4,23 @@
 int main(void)
 
 {
-    
+    int x;
+    do
+    {
+        x = get_int("Write a number from 1 to 8: \n");
+    }
+    while (x < 1 || x > 8);
+
+    for (int i = 0; i < x; i++)
+    {
+        for (int d = x - 1; d > 1; d--)
+        {
+            printf(" ");
+        }
+        for (int h = -1; h < 1; h++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
 }
