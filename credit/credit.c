041da2ee2cd0721a6d7958a7d388a7aef501f4d9
lu-carvalho <91640075+lu-carvalho@users.noreply.github.com>
2022-01-07 17:33:17 +0000

    Fri, Jan 7, 2022, 5:33 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -7,7 +7,7 @@ int main(void)
     long n;
     n = get_long("Number: \n");
 
-    //count length of the number to see if its addequate
+    //count length of the number
     int i = 0;
     int cc = n;
     while (cc > 0)
@@ -15,4 +15,15 @@ int main(void)
         cc = cc/10;
         i++;
     }
+
+    //having the length, we'll check if it's valid considering the 3 possible lenghts
+
+    if (i != 13 && i != 15 && i != 16)
+    {
+        printf("INVALID\n");
+        return 0;
+    }
+
+    //if the lengh is adequate, the sum must be calculated
+    
 }
