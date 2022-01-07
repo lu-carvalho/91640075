7c049ac909013684211dd2eec0a847e4cc69b821
lu-carvalho <91640075+lu-carvalho@users.noreply.github.com>
2022-01-07 17:29:51 +0000

    Fri, Jan 7, 2022, 5:29 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -3,5 +3,16 @@
 
 int main(void)
 {
-    
+    //get the card number from the user
+    long n;
+    n = get_long("Number: \n");
+
+    //count length of the number to see if its addequate
+    int i = 0;
+    int cc = n;
+    while (cc > 0)
+    {
+        cc = cc/10;
+        i++;
+    }
 }
