176207f3055d17d841bce2503ea0d6bce35600ce
lu-carvalho <91640075+lu-carvalho@users.noreply.github.com>
2022-01-13 02:46:38 +0000

    Thu, Jan 13, 2022, 2:46 AM +00:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -5,6 +5,9 @@
 
 // Points assigned to each letter of the alphabet
 int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+int SMALL_LETTERS[] = {97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122};
+int UPPER_LETTERS[] = {65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90};
+
 
 int compute_score(string word);
 
@@ -27,6 +30,6 @@ int compute_score(string word)
     int score = 0;
     for(int i = 0; i < strlen(word); i++)
     {
-        if 
+        if
     }
 }
