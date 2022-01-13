d56b1502277a850df8c784505049e5d5f1175b30
lu-carvalho <91640075+lu-carvalho@users.noreply.github.com>
2022-01-13 01:49:08 +0000

    Thu, Jan 13, 2022, 1:49 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -9,9 +9,7 @@ int main(void)
 {
     //get a string from the user
     string text;
-    text = get_string("Text: \n");
-
-    printf("%s\n", text);
+    text = get_string("Text: ");
 
     count_letters(text);
 }
