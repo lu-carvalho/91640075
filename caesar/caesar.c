4f32bc56dcfbcae56252d2ae01f74bef4e0e33e0
lu-carvalho <91640075+lu-carvalho@users.noreply.github.com>
2022-01-17 14:37:47 +0000

    Mon, Jan 17, 2022, 2:37 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -15,10 +15,10 @@ int main(int argc, string argv[])
     }
 
     //if the user prompts a non valid key, the program should remind it how to use the program
-    string k = argv[1];
-    for (int i = 0; i < strlen(k); i++)
+    string key = argv[1];
+    for (int i = 0; i < strlen(key); i++)
     {
-        if (!isdigit(k[i]))
+        if (!isdigit(key[i]))
         {
             printf("Usage: ./caesar key\n");
             return 1;
@@ -29,8 +29,26 @@ int main(int argc, string argv[])
     string pt = get_string("plaintext:  ");
 
     //the program calculates the cyphertext, considering if the character is upper or lowercase
+    int k = atoi(key);
+    printf("ciphertext: ");
 
+    for (int i = 0; i < strlen(plaintext); i++)
+    {
+        if (isupper(plaintext[i]))
+        {
+            printf("%c", (((plaintext[i] - 65) + k) %26) + 65);
+        }
 
+        else if (islower(plaintext[i]))
+        {
+            printf("%c", (((plaintext[i] - 97) + k) %26) + 97);
+        }
+
+        else
+        {
+            printf("%c", (plaintext[i]));
+        }
+    }
 
 
 
