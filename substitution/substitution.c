b65ad3dc66bec2db637e913bacc9f255264e1f9c
lu-carvalho <91640075+lu-carvalho@users.noreply.github.com>
2022-01-17 11:35:27 +0000

    Mon, Jan 17, 2022, 11:35 AM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -3,10 +3,10 @@
 #include <string.h>
 #include <ctype.h>
 
-int main (int argv, string agrv[])
+int main (int argc, string agrv[])
 {
     //make sure the user is inputing only one command line argument
-    if (argv != 2)
+    if (argc != 2)
     {
         printf("Usage: ./substitution key\n");
         return 1;
