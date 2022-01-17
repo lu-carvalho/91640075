d0507523daf07251716db1b6e04df886d0e36590
lu-carvalho <91640075+lu-carvalho@users.noreply.github.com>
2022-01-17 11:56:46 +0000

    Mon, Jan 17, 2022, 11:56 AM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -4,4 +4,9 @@
 #include <cstype.h>
 #include <math.h>
 
-
+int main(int argc, string argv[])
+{
+    //the user must prompt the key right when executing the program
+    //the program now asks the user to prompt the plaintext
+    //the program gives back to the user the cyphertext
+}
