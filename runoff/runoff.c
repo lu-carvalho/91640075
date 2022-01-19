8a501c4be0a1e1569fd92db8e6e066085057b122
lu-carvalho <91640075+lu-carvalho@users.noreply.github.com>
2022-01-19 14:15:02 +0000

    Wed, Jan 19, 2022, 2:15 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -142,7 +142,13 @@ bool vote(int voter, int rank, string name)
 // Tabulate votes for non-eliminated candidates
 void tabulate(void)
 {
-    // TODO
+    for (int i = 0; i < voter_count; i++)
+    {
+        for (int j = 0; j < candidate_count; j++)
+        {
+            
+        }
+    }
     return;
 }
 
