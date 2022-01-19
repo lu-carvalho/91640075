c43557239e3c71da8c69f224d42d9bb2425fd625
lu-carvalho <91640075+lu-carvalho@users.noreply.github.com>
2022-01-19 13:59:17 +0000

    Wed, Jan 19, 2022, 1:59 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -129,9 +129,9 @@ bool vote(int voter, int rank, string name)
 {
     for (int i = 0; i < candidate_count; i++)
     {
-        if (strcmp(candidates.[i].name, name) == 0)
+        if (strcmp(candidates[i].name, name) == 0)
         {
-            candidate_count++;
+            preferences
         }
     }
 
