48ab1e476cd56eb91e56db5600ea1d264ce47dad
lu-carvalho <91640075+lu-carvalho@users.noreply.github.com>
2022-01-19 14:32:58 +0000

    Wed, Jan 19, 2022, 2:32 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -188,11 +188,18 @@ int find_min(void)
 // Return true if the election is tied between all candidates, false otherwise
 bool is_tie(int min)
 {
+    int eliminate = 0;
+    int counter = 0;
+
     for (int i = 0; i < candidate_count; i++)
     {
         if (candidates[i].eliminated == false)
         {
-            return true;
+            eliminate++;
+        }
+        if (candidates[i].votes == min)
+        {
+            counter++
         }
     }
     return false;
