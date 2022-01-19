5378ce51ba95ec8b96c82bdd4b8f252617152f2e
lu-carvalho <91640075+lu-carvalho@users.noreply.github.com>
2022-01-19 12:53:34 +0000

    Wed, Jan 19, 2022, 12:53 PM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -82,5 +82,14 @@ bool vote(string name)
 void print_winner(void)
 {
     // TODO
+    int maximum_vote == 0;
+
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].votes > maximum_vote)
+        {
+            maximum_vote == candidates[i].votes;
+        }
+    }
     return;
 }
