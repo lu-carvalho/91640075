4598d714ec17eb79b8bd351a077fa07671d9d21f
lu-carvalho <91640075+lu-carvalho@users.noreply.github.com>
2022-02-01 10:57:03 +0000

    Tue, Feb 1, 2022, 10:57 AM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -11,7 +11,8 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j < width; j++)
         {
             double average = (image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue)/3;
-            round(average)
+            round(average);
+            
 
         }
     }
