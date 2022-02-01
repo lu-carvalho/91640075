359516af6457183aa2d7c829eaa1c07d5c04d398
lu-carvalho <91640075+lu-carvalho@users.noreply.github.com>
2022-02-01 11:32:47 +0000

    Tue, Feb 1, 2022, 11:32 AM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -61,7 +61,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            image[i][j] = image[i]
+            image[i][j] = image[i][width - j];
         }
     }
     return;
