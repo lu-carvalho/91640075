ebac66ae4d2858f243f447c80b495e874a6f4c04
lu-carvalho <91640075+lu-carvalho@users.noreply.github.com>
2022-02-02 23:04:13 +0000

    Wed, Feb 2, 2022, 11:04 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <cs50.h>
+
+const int JPEG_SIZE = 512
 
 int main(int argc, char *argv[])
 {
  FILE *f = fopen("card.raw", "r");
 
- int jpegs[512];
- fread(jpegs; 512bytes; idontknow; )
+ int i =0
+ int jpegs[i];
+
+ fread(jpegs; JPEG_SIZE; 1; FILE *f);
+
+
+
+
 }
