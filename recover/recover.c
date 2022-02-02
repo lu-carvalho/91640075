9acbbba4c1ed8d80955d68bcb9ed5fc319b49063
lu-carvalho <91640075+lu-carvalho@users.noreply.github.com>
2022-02-02 23:20:55 +0000

    Wed, Feb 2, 2022, 11:20 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -9,10 +9,15 @@ int main(int argc, char *argv[])
 {
     if (argc != 2)
     {
-        printf("usage ./recover IMAGE\n");
+        printf("usage: ./recover IMAGE\n");
         return 1;
     }
-    
-    FILE *file = fopen(argv[1], "r");
+
+    FILE *image = fopen(argv[1], "r");
+
+    int i = 0;
+    int files[i];
+
+    fread(files, sizeof(BYTE), 512, image);
 
 }
