97e76d900c57bd50ef9ca893aafd5583cd914776
lu-carvalho <91640075+lu-carvalho@users.noreply.github.com>
2022-02-10 19:58:12 +0000

    Thu, Feb 10, 2022, 7:58 PM +00:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -17,6 +17,9 @@ def main():
     teams = []
     # TODO: Read teams into memory from file
 
+    with open (sys.argv[2],"r") as file:
+        
+
     counts = {}
     # TODO: Simulate N tournaments and keep track of win counts
 
