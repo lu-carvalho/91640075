4505978d552042a5e41760e2d5c8a3c7d36de15f
lu-carvalho <91640075+lu-carvalho@users.noreply.github.com>
2022-02-10 20:42:07 +0000

    Thu, Feb 10, 2022, 8:42 PM +00:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -2,11 +2,11 @@ from cs50 import get_int
 
 while True:
     height = get_int("Height: ")
-    if height>0:
+    if height>0 and height<9:
         break
 
 for i in range(height):
-    
+
     print(" "*(height-1), end="")
     print("#" * (height-1))
     height = height - 1
