3cc66389199ca2507fe29c01ca3bc70593ed940d
lu-carvalho <91640075+lu-carvalho@users.noreply.github.com>
2022-02-10 20:33:32 +0000

    Thu, Feb 10, 2022, 8:33 PM +00:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -3,5 +3,6 @@ from cs50 import get_int
 height = get_int("Height: ")
 
 for i in range(height):
-    print("#" * height)
+    print(" " * (height-1))
+    print("#" * (height - (height-1)))
     height = height - 1
