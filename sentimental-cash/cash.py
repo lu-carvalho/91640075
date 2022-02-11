0aef81e393e2e6b6d3a715519b5d886605a386a2
lu-carvalho <91640075+lu-carvalho@users.noreply.github.com>
2022-02-11 12:51:57 +0000

    Fri, Feb 11, 2022, 12:51 PM +00:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -11,9 +11,30 @@ while True:
 
 # spits out the minimum number of coins with which said change can be made
 while change >= 0.25:
-    change -= 1
+    change -= 0.25
     coins += 1
+    if change < 0.25:
+        break
 
+while change >= 0.10:
+    change -= 0.10
+    coins += 1
+    if change < 0.10:
+        break
+
+while change >= 0.05:
+    change -= 0.05
+    coins += 1
+    if change < 0.05:
+        break
+
+while change >= 0.01:
+    change -= 0.01
+    coins += 1
+    if change < 0.01:
+        break
+
+print("{}".format, coins)
 # assume that the user will input their change in dollars
 
 #  Assume that the only coins available are quarters (25¢), dimes (10¢), nickels (5¢), and pennies (1¢).
