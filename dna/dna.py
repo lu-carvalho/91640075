fdf4929eb43165a69a383e87375e754e968dd1cb
lu-carvalho <91640075+lu-carvalho@users.noreply.github.com>
2022-02-11 23:49:20 +0000

    Fri, Feb 11, 2022, 11:49 PM +00:00

--- dna/dna.py
+++ dna/dna.py
@@ -16,7 +16,7 @@ def main():
 
     # TODO: Read DNA sequence file into a variable
     with open(sys.argv[2], "r") as txtfile:
-        sequence = file.read()
+        sequence = txtfile.read()
 
     # TODO: Find longest match of each STR in DNA sequence
     STRs = []
