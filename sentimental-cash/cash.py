cb36a14fa4423551778433aaf1731b00d2ce2a94
lu-carvalho <91640075+lu-carvalho@users.noreply.github.com>
2022-02-11 12:44:12 +0000

    Fri, Feb 11, 2022, 12:44 PM +00:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -3,6 +3,12 @@ from cs50 import get_float
 # asks the user how much change is owed
 # force him to give me a valid number
 
+while True:
+    change = get_float("Change owed: ")
+    if change > 0:
+        break
+
+
 # spits out the minimum number of coins with which said change can be made
 
 # assume that the user will input their change in dollars
