965af8700ab32bdb887c6605331c68a2d0a5bea0
lu-carvalho <91640075+lu-carvalho@users.noreply.github.com>
2022-02-11 12:41:43 +0000

    Fri, Feb 11, 2022, 12:41 PM +00:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -1,6 +1,10 @@
 from cs50 import get_float
 
 # asks the user how much change is owed
+# force him to give me a valid number
 
 # spits out the minimum number of coins with which said change can be made
+
 # assume that the user will input their change in dollars
+
+#  Assume that the only coins available are quarters (25¢), dimes (10¢), nickels (5¢), and pennies (1¢).
