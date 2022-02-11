e8d5b41faaaea9047ed8ddcee1d8ca8e71ae4e4f
lu-carvalho <91640075+lu-carvalho@users.noreply.github.com>
2022-02-11 12:45:51 +0000

    Fri, Feb 11, 2022, 12:45 PM +00:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -9,7 +9,7 @@ while True:
         break
 
 # spits out the minimum number of coins with which said change can be made
-
+while change%25 
 
 # assume that the user will input their change in dollars
 
