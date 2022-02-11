cee1618dd7768600307ec79837c61cff014bd617
lu-carvalho <91640075+lu-carvalho@users.noreply.github.com>
2022-02-11 01:25:43 +0000

    Fri, Feb 11, 2022, 1:25 AM +00:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -1,10 +1,10 @@
-from cs50 import get_int
-
+# forces the user to input a valid answer
 while True:
-    height = get_int("Height: ")
-    if height>0 and height<9:
+    height = int(input("Height: "))
+    if height > 0 and height < 9:
         break
 
+# prints the pyramid piramid pirâmide
 for i in range(height):
     print((height - 1 - i) * " ", end="")
     print((i + 1) * "#")
