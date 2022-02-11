bb4a1889363ebb026e6c47e75e41f477d85eaa91
lu-carvalho <91640075+lu-carvalho@users.noreply.github.com>
2022-02-11 23:27:06 +0000

    Fri, Feb 11, 2022, 11:27 PM +00:00

--- dna/dna.py
+++ dna/dna.py
@@ -10,8 +10,7 @@ def main():
         sys.exit(1)
 
     # TODO: Read database file into a variable
-    with open(sys.argv[1]),"r") as csv:
-        
+    with open(sys.argv[1]),"r") as csv
 
     # TODO: Read DNA sequence file into a variable
 
