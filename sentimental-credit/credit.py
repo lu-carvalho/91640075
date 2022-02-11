1b2dab6fe3d3f18ff5f969b7bf304fd800ca63f7
lu-carvalho <91640075+lu-carvalho@users.noreply.github.com>
2022-02-11 13:55:34 +0000

    Fri, Feb 11, 2022, 1:55 PM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -4,15 +4,26 @@ import cs50
 # define main as the function that will use the other functions
 
 def main():
-    card_number = get_card_number()
+    credit_card = get_card_number()
 
-    validade_card(credit_number)
-
-
+    validade_card(credit_card)
 
 # prompt the user for the credit card number
 
+def get_card_number():
+    while True:
+        card_num = input("Number: ")
+        try:
+            if len(card_num) > 0 and int(card_num):
+                break
+        except ValueError:
+            continue
+    return(card_num)
+
 # define a function that will validade the credit card number
+
+def validade_card(credit_card):
+    if len(credit_card) < 
 
 if __name__ == "__main__":
     main()
