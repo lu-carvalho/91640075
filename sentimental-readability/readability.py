f4cf7208db5adece9786f131ec82b3c4cc7d364d
lu-carvalho <91640075+lu-carvalho@users.noreply.github.com>
2022-02-11 14:53:06 +0000

    Fri, Feb 11, 2022, 2:53 PM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -6,6 +6,9 @@ def main():
     print(grade_level(text))
 
 # i need to count the letters, the words and the sentences
+def count_letters(text):
+    for i in range(len(text)):
+        if(text[i])
 
 
 # after that, with the number of each of them, i need to make the calculus
