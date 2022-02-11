d5acb98ed051a13ca014f21a7db5e052f8c6d1e0
lu-carvalho <91640075+lu-carvalho@users.noreply.github.com>
2022-02-11 12:59:12 +0000

    Fri, Feb 11, 2022, 12:59 PM +00:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -5,36 +5,28 @@ coins = 0
 # asks the user how much change is owed
 # force him to give me a valid number
 while True:
-    change = get_float("Change owed: ")
-    if change > 0:
+    change_dol = get_float("Change owed: ")
+    if change_dol > 0:
         break
 
+change = round(int(change_dol * 100))
+
 # spits out the minimum number of coins with which said change can be made
-if change >= 0.25:
-    change = change - 0.25
-    coins += 1
-    #if change < 0.25:
-        #break
+while change > 0:
+    while change > 0.25:
+        change -= 0.25
+        coins += 1
+    while change > 0.10:
+        change -= 0.10
+        coins += 1
+    while change > 0.05:
+        change -= 0.05
+        coins += 1
+    while change > 0.01:
+        change -= 0.01
+        coins += 1
+print(coins)
 
-if change >= 0.10:
-    change = change - 0.10
-    coins += 1
-    #if change < 0.10:
-        #break
-
-if change >= 0.05:
-    change = change - 0.05
-    coins += 1
-    #if change < 0.05:
-        #break
-
-if change >= 0.01:
-    change = change - 0.01
-    coins += 1
-    #if change < 0.01:
-        #break
-
-print("{}".format, coins)
 # assume that the user will input their change in dollars
 
 #  Assume that the only coins available are quarters (25¢), dimes (10¢), nickels (5¢), and pennies (1¢).
