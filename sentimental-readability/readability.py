8daa1aae74a66070a7286c60ca2a50622b6b96cf
lu-carvalho <91640075+lu-carvalho@users.noreply.github.com>
2022-02-11 14:37:09 +0000

    Fri, Feb 11, 2022, 2:37 PM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -1,2 +1,15 @@
 from cs50 import get_string
 
+# main: gets input and gives grade back
+def main():
+    text = get_string("Text: ")
+    print(grade_level(text))
+
+# define function that will calculate the grade level
+def grade_level():
+    
+
+
+
+if __name__ == "__main__":
+    main()
