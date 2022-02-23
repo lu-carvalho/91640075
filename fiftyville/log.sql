7044ef55d647b2670e3930f6bbadca47489f26b3
lu-carvalho <91640075+lu-carvalho@users.noreply.github.com>
2022-02-23 14:44:10 +0000

    Wed, Feb 23, 2022, 2:44 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,2 +1,5 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
-
+-- get the description of the crime
+SELECT description FROM crime_scene_reports
+WHERE month = 7 AND day = 28
+AND street = "Humphrey Street";
