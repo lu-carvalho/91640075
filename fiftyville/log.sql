08e52ba9c506ffafe3180451ba9ade8a8672f125
lu-carvalho <91640075+lu-carvalho@users.noreply.github.com>
2022-02-23 15:05:30 +0000

    Wed, Feb 23, 2022, 3:05 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -8,4 +8,5 @@ AND street = "Humphrey Street";
 -- get the transcripts from the interviews
 SELECT id, name, transcript FROM interviews WHERE year = 2021 AND month = 7 AND day = 28;
 
--- 
+-- get the license plates that left the bakery around 10 minutes after 10h15
+SELECT license_plate, activity, minute FROM bakery_security_logs WHERE year = 2021 AND month = 7 AND day = 28 AND hour = 10;
