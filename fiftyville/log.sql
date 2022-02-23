9fef57d5f2735c93bf06e2e8dd56de4bcf31ea1b
lu-carvalho <91640075+lu-carvalho@users.noreply.github.com>
2022-02-23 15:03:07 +0000

    Wed, Feb 23, 2022, 3:03 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,17 +1,11 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
+
 -- get the description of the crime
 SELECT description FROM crime_scene_reports
 WHERE month = 7 AND day = 28
 AND street = "Humphrey Street";
 
---OUTPUT: Theft of the CS50 duck took place at 10:15am at the Humphrey Street bakery. Interviews were conducted today with three
---witnesses who were present at the time – each of their interview transcripts mentions the bakery.Littering took place at 16:36.
---No known witnesses.
-
--- Select the activity from the bakery at the day the crime happened
-SELECT id, activity, hour, minute FROM bakery_security_logs
-WHERE year = 2021 AND month = 7 AND day = 28;
-
--- the IDs 258 and 259 are suspects. I
-
+-- get the transcripts from the interviews
 SELECT id, name, transcript FROM interviews WHERE year = 2021 AND month = 7 AND day = 28;
+
+-- 
