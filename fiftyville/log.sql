b92b8745181ad8009bd32ab71dfbb198eb73d1d9
lu-carvalho <91640075+lu-carvalho@users.noreply.github.com>
2022-02-23 14:49:52 +0000

    Wed, Feb 23, 2022, 2:49 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -4,7 +4,10 @@ SELECT description FROM crime_scene_reports
 WHERE month = 7 AND day = 28
 AND street = "Humphrey Street";
 
---Theft of the CS50 duck took place at 10:15am at the Humphrey Street bakery. Interviews were conducted today with three
+--OUTPUT: Theft of the CS50 duck took place at 10:15am at the Humphrey Street bakery. Interviews were conducted today with three
 --witnesses who were present at the time – each of their interview transcripts mentions the bakery.Littering took place at 16:36.
 --No known witnesses.
 
+-- Select the activity from the bakery at the moment the crime happened
+SELECT activity FROM bakery_security_logs
+WHERE year = 2021 AND month = 7 AND day = 28 AND hour = 10 AND minute = 15;
