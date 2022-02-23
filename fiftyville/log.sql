cdcbb2285d7d261aef856d5d051f468d76d40225
lu-carvalho <91640075+lu-carvalho@users.noreply.github.com>
2022-02-23 15:12:33 +0000

    Wed, Feb 23, 2022, 3:12 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -9,7 +9,10 @@ AND street = "Humphrey Street";
 SELECT id, name, transcript FROM interviews WHERE year = 2021 AND month = 7 AND day = 28;
 
 -- get the license plates that left the bakery around 10 minutes after 10h15
-SELECT license_plate, activity, minute FROM bakery_security_logs WHERE year = 2021 AND month = 7 AND day = 28 AND hour = 10;
+SELECT license_plate, activity, minute FROM bakery_security_logs WHERE year = 2021 AND month = 7 AND day = 28 AND hour = 10 AND minute > 15 AND minute < 26;
 
 -- if the thief left around 10 minutes after the theft, than we have the following license plates that are suspicious:
 -- 5P2BI95 (1 min), 94KL13X (3min), 6P58WS2 (3min), 4328GD8 (4min), G412CB7(5 min), L93JTIZ(6 min), 322W7JE(8min), 0NTHK55(8min)
+-- what are their names?
+
+SELECT name FROM people WHERE (SELECT)
