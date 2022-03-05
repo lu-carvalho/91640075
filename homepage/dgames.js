4ec3db368e089a1db5297a34f16acdc22c176a22
lu-carvalho <91640075+lu-carvalho@users.noreply.github.com>
2022-03-05 00:52:34 +0000

    Sat, Mar 5, 2022, 12:52 AM +00:00

--- homepage/dgames.js
+++ homepage/dgames.js
@@ -1,5 +1,7 @@
-var todbutton = document.getElementById("todbutton");
-
-todbutton.onclick = function(){
+document.getElementById("todbutton").onclick = function(){
     location.href = "truthordare.html"
+};
+
+document.getElementById("nhiebutton").onclick = function(){
+    location.href = "neverhiever.html"
 };
