e555f7de51b2470f1a3ab45db0c4a162ac47e081
lu-carvalho <91640075+lu-carvalho@users.noreply.github.com>
2022-03-05 18:34:03 +0000

    Sat, Mar 5, 2022, 6:34 PM +00:00

--- homepage/homepage.js
+++ homepage/homepage.js
@@ -1,3 +1,7 @@
 document.getElementById("page2").onclick = function(){
     location.href = "favfood.html"
 };
+
+document.getElementById("page3").onclick = function(){
+    location.href = "favmovies.html"
+};
