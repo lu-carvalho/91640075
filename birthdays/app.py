bc55957d612026334972a807fbd5db06b2c3c1ad
lu-carvalho <91640075+lu-carvalho@users.noreply.github.com>
2022-03-08 14:12:13 +0000

    Tue, Mar 8, 2022, 2:12 PM +00:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -33,6 +33,7 @@ def index():
     else:
 
         # TODO: Display the entries in the database on index.html -- for this, we need to get access to all the peoples birthdays inside the database so i have to add logic to query the database here
+        birthdays.execute 
 
         return render_template("index.html")
 
