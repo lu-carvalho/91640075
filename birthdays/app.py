68f9c761f80f5c0e1d73045f5181ba960ee14f1c
lu-carvalho <91640075+lu-carvalho@users.noreply.github.com>
2022-03-08 14:53:02 +0000

    Tue, Mar 8, 2022, 2:53 PM +00:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -27,7 +27,8 @@ def index():
     if request.method == "POST":
 
         # TODO: Add the user's entry into the database
-
+        new_birthday = db.execute("INSERT ? ")
+        
         return redirect("/")
 
     else:
