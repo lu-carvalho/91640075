0b07045da2b4edef3bbb8740bf8c0ddbf6eff4e2
lu-carvalho <91640075+lu-carvalho@users.noreply.github.com>
2022-03-09 10:57:59 +0000

    Wed, Mar 9, 2022, 10:57 AM +00:00

--- finance/app.py
+++ finance/app.py
@@ -117,7 +117,7 @@ def quote():
             return apology("That stock doesn't exist", 403)
 
         else:
-            return redirect("/quoted", name="companyName", price="price")
+            return redirect("/quoted")
 
     else:
         return render_template("quote.html")
