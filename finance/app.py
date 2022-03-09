f92665f58d7d744ee0190765281340418880cd72
lu-carvalho <91640075+lu-carvalho@users.noreply.github.com>
2022-03-09 16:27:11 +0000

    Wed, Mar 9, 2022, 4:27 PM +00:00

--- finance/app.py
+++ finance/app.py
@@ -29,6 +29,11 @@ db = SQL("sqlite:///finance.db")
 if not os.environ.get("API_KEY"):
     raise RuntimeError("API_KEY not set")
 
+# create table and index in order to keep track of each user stock orders
+db.execute("CREATE TABLE IF NOT EXISTS orders (id INTEGER, user_id NUMERIC NOT NULL, symbol TEXT NOT NULL, shares NUMERIC NOT NULL, price NUMERIC NOT NULL, timestamp TEXT, PRIMARY KEY(id), FOREIGN KEY(user_id) REFERENCES users(id))")
+
+db.execute("CREATE INDEX IF NOT EXISTS orders_by_user_id_index ON orders (user_id)")
+
 
 @app.after_request
 def after_request(response):
